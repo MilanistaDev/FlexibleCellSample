ae4710d2976ddb90ca1866d8bb75ea02cbea5ac3
Takuya Aso <milanista224@gmail.com>
2020-08-31 01:30:38 +0900

    Initial Commit

---
+++ FlexibleCellSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlexibleCellSample
+//
+//  Created by Takuya Aso on 2020/08/31.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
